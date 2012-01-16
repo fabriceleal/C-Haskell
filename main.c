8e0e781c7d83b32f58af601a01385ce1f215e57e
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-16 13:55:20 -0800

    main ok

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
+#include <HsFFI.h>
+#ifdef __GLASGOW_HASKELL__
+#include "fibo_stub.h"
+extern void __stginit_Safe(void);
+#endif
+
 #include <stdio.h>
-
-
-int to_call(void * arg)
-{
-	printf("C, Called from haskell\n");
-
-	return 0xDEADBEAF;
-}
 
 
 int main(int argc, char* argv[])
 {
-	printf("Calling haskell, from C ...\n");
+	int i;
+	hs_init(&argc, &argv);
 
+	#ifdef __GLASGOW_HASKELL__
+
+	hs_add_root(__stginit_Safe);
+
+	#endif
+
+	i = fibonacci_hs(42);
+	printf("Fibonacci: %d\n", i);
+
+	hs_exit();
 	
 	return 0x0;
 }
