27b0b335b9c9e9e87519d2fb436565f21d22302f
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-16 14:12:30 -0800

    main source file, TODO comments

--- ex01/main.c
+++ ex01/main.c
@@ -2,6 +2,7 @@
 
 #ifdef __GLASGOW_HASKELL__
 
+// TODO: Explain this
 	#include "fibo_stub.h"
 	extern void __stginit_Safe(void);
 
@@ -13,10 +14,13 @@
 int main(int argc, char* argv[])
 {
 	int i;
+
+	// TODO: Explain this
 	hs_init(&argc, &argv);
 
 	#ifdef __GLASGOW_HASKELL__
 
+	// TODO: Explain this
 	hs_add_root(__stginit_Safe);
 
 	#endif
@@ -24,6 +28,7 @@ int main(int argc, char* argv[])
 	i = fibonacci_hs(42);
 	printf("Fibonacci: %d\n", i);
 
+	// TODO: Explain this
 	hs_exit();
 	
 	return 0x0;
