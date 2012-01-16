34c26c6454395ac60b8c5f5422073fa2c2a8e596
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-16 14:11:14 -0800

    git ignore changed, fibo commented

--- ex01/fibo.hs
+++ ex01/fibo.hs
@@ -4,12 +4,14 @@ module Safe where
 
 import Foreign.C.Types
 
-
+{-# fibo Haskell definition #-}
 fibonacci :: Int -> Int
 fibonacci n = fibs !! n
   where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
+{-# fibo Haskell C interface to fibo Haskell definition #-}
 fibonacci_hs :: CInt -> CInt
 fibonacci_hs = fromIntegral . fibonacci . fromIntegral
 
+{-# export the Haskell C interface #-}
 foreign export ccall fibonacci_hs :: CInt -> CInt
